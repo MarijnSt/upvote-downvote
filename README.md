d716048de0905842b6ec9cf3bff7ea758c318b6e
MarijnSt <marijn.stammeleer@gmail.com>
2019-06-26 14:33:11 +0200

    finished

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-This is part of a project where we need to like and dislike posts.
+This small app will be used in another project to like and dislike posts.
+
+## TODO:
+    *   Link to API
--- src/index.js
+++ src/index.js
@@ -4,20 +4,71 @@ import './index.css';
 
 /* Images */
 import up from './images/up.svg';
+import upClicked from './images/up-clicked.svg';
 import down from './images/down.svg';
+import downClicked from './images/down-clicked.svg';
 
 const App = () => {
-    const [upvotes, setUp] = useState(0);
-    const [downvotes, setDowns] = useState(0);
+    const [ups, setUps] = useState({ votes: 0, status: false });
+    const [downs, setDowns] = useState({ votes: 0, status: false });
+
+    /* change colors */
+    const resetUp = () => {
+        setUps({ votes: ups.votes - 1, status: false })
+        document.querySelector('#upIcon').setAttribute('src', up);
+        document.querySelector('#upText').style.color = "#000000";
+    }
+
+    const activateUp = () => {
+        setUps({ votes: ups.votes + 1, status: true });
+        document.querySelector('#upIcon').setAttribute('src', upClicked);
+        document.querySelector('#upText').style.color = "#00FF85";
+    }
+
+    const resetDown = () => {
+        setDowns({ votes: downs.votes - 1, status: false });
+        document.querySelector('#downIcon').setAttribute('src', down);
+        document.querySelector('#downText').style.color = "#000000";
+    }
+
+    const activateDown = () => {
+        setDowns({ votes: downs.votes + 1, status: true });
+        document.querySelector('#downIcon').setAttribute('src', downClicked);
+        document.querySelector('#downText').style.color = "#FF007A";
+    }
+
+    const handleUpvote = () => {
+        if(downs.status === true) {
+            resetDown();
+        }
+
+        if(ups.status === false) {
+            activateUp();
+        } else if (ups.status === true) {
+            resetUp();
+        }
+    }
+
+    const handleDownvote = () => {
+        if(ups.status === true) {
+            resetUp();
+        }
+
+        if(downs.status === false) {
+            activateDown();
+        } else if (downs.status === true) {
+            resetDown();
+        }
+    }
 
     return (
         <div className="main-container">
             <h1>Duimpjestest</h1>
             <div className="thumb-container">
-                <img src={up} onClick={() => setUp(upvotes + 1)}/>
-                <p>{upvotes}</p>
-                <img src={down} onClick={() => setDowns(downvotes + 1)}/>
-                <p>{downvotes}</p>
+                <img id="upIcon" src={up} onClick={() => handleUpvote()}/>
+                <p id="upText">{ups.votes}</p>
+                <img id="downIcon" src={down} onClick={() => handleDownvote()}/>
+                <p id="downText">{downs.votes}</p>
             </div>
         </div>
     )
