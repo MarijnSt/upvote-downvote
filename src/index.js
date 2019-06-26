c9f3a0f15fb5b474609067c37ab39b2dc0c4d4f9
MarijnSt <marijn.stammeleer@gmail.com>
2019-06-26 10:48:51 +0200

    first commit, voting works but not yet limited

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+/* Images */
+import up from './images/up.svg';
+import down from './images/down.svg';
+
+const App = () => {
+    const [upvotes, setUp] = useState(0);
+    const [downvotes, setDowns] = useState(0);
+
+    return (
+        <div className="main-container">
+            <h1>Duimpjestest</h1>
+            <div className="thumb-container">
+                <img src={up} onClick={() => setUp(upvotes + 1)}/>
+                <p>{upvotes}</p>
+                <img src={down} onClick={() => setDowns(downvotes + 1)}/>
+                <p>{downvotes}</p>
+            </div>
+        </div>
+    )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
